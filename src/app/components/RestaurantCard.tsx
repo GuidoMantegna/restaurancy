6ca15a43962f12550855891c514a0fc8776941b0
Guido Mantegna <Guido>
2025-10-23 16:11:55 -0300

    Create dynamic route for restaurants & RestaurantCard component

---
+++ src/app/components/RestaurantCard.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import type { Restaurant } from "../../types";
+export default function RestaurantPage(restaurant: Restaurant) {
+  return (
+    <article key={restaurant.id} className="max-w-[50%] h-full m-auto">
+      <div className="relative flex justify-center items-center">
+        <img
+          alt={restaurant.name}
+          className="w-full mb-2 object-cover brightness-50 rounded-t-md"
+          src={restaurant.image}
+        />
+        <h2 className="flex flex-col items-center text-5xl font-light absolute">
+          <span>{restaurant.name}</span>
+          <small className="flex gap-2 text-base justify-end w-full mt-2 mr-6">
+            <span>⭐</span>
+            <span>{restaurant.score}</span>
+            <span className="font-normal opacity-75">
+              ({restaurant.ratings})
+            </span>
+          </small>
+        </h2>
+      </div>
+      <div className="p-5 border-x border-b border-stone-700 rounded-b-md">
+        <p className="opacity-90 font-light">{restaurant.description}</p>
+      </div>
+    </article>
+  );
+}
