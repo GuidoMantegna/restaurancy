856fc94b165e543cc61636c6978b68e093772769
Guido Mantegna <Guido>
2025-11-04 09:19:23 -0300

    Refactor 'RestaurantCard'

--- src/app/(index)/components/RestaurantCard.tsx
+++ src/app/(index)/components/RestaurantCard.tsx
@@ -2,32 +2,52 @@
 
 import Link from "next/link";
 import type { Restaurant } from "../../../types";
+import { usePathname } from "next/navigation";
+
+const TRANSITIONS_STYLES = `transition-all duration-300 ease-in-out`;
 export default function RestaurantPage(restaurant: Restaurant) {
+  const isFavourite = window.localStorage
+    .getItem("favorites")
+    ?.includes(restaurant.id);
+
+  const pathname = usePathname();
+
   return (
-    <div className="flex flex-col items-center gap-4">
-    <article key={restaurant.id} className="max-w-[50%] h-full">
-      <div className="relative flex justify-center items-center">
+    <>
+      <article
+        key={restaurant.id}
+        className={`max-w-[500px] border border-stone-700 rounded-md p-5 hover:bg-stone-800 hover:border-none ${TRANSITIONS_STYLES}`}
+      >
         <img
           alt={restaurant.name}
-          className="w-full mb-2 object-cover brightness-50 rounded-t-md"
+          className={`mb-3 h-[300px] w-full object-cover rounded-md hover:scale-102 ${TRANSITIONS_STYLES} grayscale hover:filter-none`}
           src={restaurant.image}
         />
-        <h2 className="flex flex-col items-center text-5xl font-light absolute">
-          <span>{restaurant.name}</span>
-          <small className="flex gap-2 text-base justify-end w-full mt-2 mr-6">
+        <h2 className="inline-flex gap-2 text-lg font-bold">
+          <Link href={`/${restaurant.id}`} key={restaurant.id}>
+            <span>{restaurant.name}</span>
+          </Link>
+          <small className="inline-flex gap-1">
             <span>⭐</span>
             <span>{restaurant.score}</span>
             <span className="font-normal opacity-75">
               ({restaurant.ratings})
             </span>
           </small>
+          <button
+            type="button"
+            className={`text-red-500 text-xl ${isFavourite ? "opacity-100" : "opacity-20"}`}
+          >
+            ♥
+          </button>
         </h2>
-      </div>
-      <div className="p-5 border-x border-b border-stone-700 rounded-b-md">
-        <p className="opacity-90 font-light">{restaurant.description}</p>
-      </div>
-    </article>
-    <Link href="/" className="border border-stone-700 rounded-md px-5 py-2">Go Back</Link>
-    </div>
+        <p className="opacity-90">{restaurant.description}</p>
+      </article>
+      {pathname !== "/" && 
+      <Link href="/" className="border border-stone-700 rounded-md px-5 py-2 m-10 bg-stone-800">
+        Go Back
+      </Link>
+      }
+    </>
   );
 }
--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -2,6 +2,7 @@ import api from "@/api";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 import Form from "next/form";
+import RestaurantCard from "./components/RestaurantCard";
 
 const TRANSITIONS_STYLES = `transition-all duration-300 ease-in-out`;
 
@@ -39,29 +40,7 @@ export default async function Home({
       <section className="grid grid-cols-1 gap-12 md:grid-cols-2 lg:grid-cols-3">
         {restaurants.map((restaurant) => {
           return (
-            <Link href={`/${restaurant.id}`} key={restaurant.id}>
-              <article
-                key={restaurant.id}
-                className={`border border-stone-700 rounded-md p-5 hover:bg-stone-800 hover:border-none ${TRANSITIONS_STYLES}`}
-              >
-                <img
-                  alt={restaurant.name}
-                  className={`mb-3 h-[300px] w-full object-cover rounded-md hover:scale-102 ${TRANSITIONS_STYLES} grayscale hover:filter-none`}
-                  src={restaurant.image}
-                />
-                <h2 className="inline-flex gap-2 text-lg font-bold">
-                  <span>{restaurant.name}</span>
-                  <small className="inline-flex gap-1">
-                    <span>⭐</span>
-                    <span>{restaurant.score}</span>
-                    <span className="font-normal opacity-75">
-                      ({restaurant.ratings})
-                    </span>
-                  </small>
-                </h2>
-                <p className="opacity-90">{restaurant.description}</p>
-              </article>
-            </Link>
+            <RestaurantCard key={restaurant.id} {...restaurant} />
           );
         })}
       </section>
